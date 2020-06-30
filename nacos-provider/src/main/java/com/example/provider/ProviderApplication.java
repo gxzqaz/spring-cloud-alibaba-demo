1cad6249f4d69a0f1fbff786cee8e58e4c83dada
aze <469648819@qq.com>
2020-06-30 10:04:33 +0800

    不使用默认的服务发现

--- nacos-provider/src/main/java/com/example/provider/ProviderApplication.java
+++ nacos-provider/src/main/java/com/example/provider/ProviderApplication.java
@@ -3,13 +3,11 @@ package com.example.provider;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author aze
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableDubbo /*开启Dubbo的注解支持*/
 public class ProviderApplication {
 
